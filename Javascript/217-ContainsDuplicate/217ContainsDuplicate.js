5c8bb6d3fc01a16afe773ca9055bbb418b969d55
Jem Dawson <thejemmeh@gmail.com>
2024-10-14 21:43:59 -0700

    217 Contains Duplicate Javascript

---
+++ Javascript/217-ContainsDuplicate/217ContainsDuplicate.js
@@ -0,0 +1,19 @@
+/* Set - Hash Table
+Make a set which can only hold unique values and compare the size of it to the length of nums 
+
+Time Complexity: O(n) 
+you have to go through the entire nums array to add it to the set
+
+Space Complexity: O(n)
+We need to make a set that has to hold up to n elements from the nums array.
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const set = new Set(nums); 
+    return set.size !== nums.length
+};
