7084131dc78e547eccece4a28644893c7d22f9c4
Jem Dawson <thejemmeh@gmail.com>
2024-11-03 20:20:56 -0800

    167 Two Sum 2 Input Array is Sorted Javascript

---
+++ Javascript/167-TwoSum2.InputArrayIsSorted/167TwoSum2.js
@@ -0,0 +1,25 @@
+/* Two Pointers
+Time Complexity: O(n)
+With two pointers you only have to go over the array once starting at the ends. 
+the pointers never cross.
+
+Space Complexity: O(1)
+we're just going over the numbers array given to us
+same amount of pointers no matter the size of the array
+*/
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+    while (left < right) {
+        let total = numbers[left] + numbers[right];
+        if (total === target)
+            return [left + 1, right + 1];
+        else if (total > target) right--;
+        else left++;
+    }    
+};
