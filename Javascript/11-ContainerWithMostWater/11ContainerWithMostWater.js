e51f8c1b08cf45e96f8ac11732ad55e6b92b658f
Jem Dawson <thejemmeh@gmail.com>
2024-11-05 22:47:29 -0800

    11 Container With Most Water Javascript

---
+++ Javascript/11-ContainerWithMostWater/11ContainerWithMostWater.js
@@ -0,0 +1,28 @@
+/*
+
+Time Complexity: O(n)
+The two pointers may have to traverse every element in the array to find the highest area.
+
+Space Complexity: O(1)
+We're just iterating over the height number array with a set number of pointers.
+
+*/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let result = 0,
+        left = 0,
+        right = height.length -1;
+
+    while (left < right){
+        let smallestSide = Math.min(height[left], height[right]);
+        let area = (right - left) * smallestSide;
+        if(area > result) result = area;
+        if(height[left] < height[right]) left++;
+        else right--;
+    }
+    return result;
+};
