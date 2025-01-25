aa9fda0d885c32f318beeb9abe3bcd3d93af171f
Jem Dawson <thejemmeh@gmail.com>
2025-01-24 22:34:05 -0800

    287

--- Javascript/287-FindTheDuplicateNumber/recodes.js
+++ Javascript/287-FindTheDuplicateNumber/recodes.js
@@ -1,3 +1,27 @@
+// #region Jan 24 2025
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let slow = 0,
+        fast = 0;
+    while (true){
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if (slow === fast)
+            break;
+    }
+    let slow2 = 0;
+    while (true){
+        slow = nums[slow];
+        slow2 = nums[slow2];
+        if (slow === slow2)
+            return slow;
+    }
+};
+
+// #endregion
 // #region Jan 21 2025
 /**
  * @param {number[]} nums
