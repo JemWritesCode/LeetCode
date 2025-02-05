f74767ec04d32f5f138002fb9e68d6204fe904c9
Jem Dawson <thejemmeh@gmail.com>
2025-02-04 22:41:46 -0800

    recode

--- Javascript/217-ContainsDuplicate/Recodes.js
+++ Javascript/217-ContainsDuplicate/Recodes.js
@@ -1,3 +1,22 @@
+// #region Feb 4 2025
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+ // time complexity: o(n) in worst case must visit every num in nums. no duplicates.
+ // space complexity: o(n) in worst case no duplicates Set will have every num of nums
+ var containsDuplicate = function(nums) {
+    const seen = new Set();
+    for (num of nums){
+        if (seen.has(num)) return true;
+        else seen.add(num);
+    }
+    return false;
+};
+
+// #endregion 
+
 // #region Feb 2 2025
 /**
  * @param {number[]} nums
