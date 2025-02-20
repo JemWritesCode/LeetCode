37328a2b2ea97d11110e93b1bcca8f751e0c2924
Jem Dawson <thejemmeh@gmail.com>
2025-02-19 22:07:37 -0800

    recode

--- Javascript/206-ReverseLinkedList/recodes.js
+++ Javascript/206-ReverseLinkedList/recodes.js
@@ -1,3 +1,32 @@
+// #region 12 -- Feb 19 2025
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+ // Time Complexity: O(n) where n is the number of nodes in the linked list. We must visit each node to reverse the link.
+ // Space Complexity: O(1) no matter the size of the linked list being passed in we just need the same 3 pointers prev, curr, and next. 
+ var reverseList = function(head) {
+    let prev = null,
+        curr = head;
+    while (curr){
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+};
+
+// #endregion 
+
 // #region 11 -- Feb 18 2025
 /**
  * Definition for singly-linked list.
