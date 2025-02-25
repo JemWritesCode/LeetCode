5620b218fe775edd7531b22d8ee3ab668287a23c
Jem Dawson <thejemmeh@gmail.com>
2025-02-24 21:31:05 -0800

    recode

--- Javascript/226-InvertBinaryTree/recodes.js
+++ Javascript/226-InvertBinaryTree/recodes.js
@@ -1,3 +1,29 @@
+// #region 2 -- Feb 24 2025
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+ 
+ // Destructuring assignment and recursion.
+ // Time Complexity: O(n) Where n is the number of nodes in the tree. We must traverse the entire tree to invert it.
+ // Space Complexity: O(h) Each recursive call adds a new frame to the call stack, which can lead to O(h) space complexity, where h is the height of the tree. In the worst case (for a skewed tree), this could be O(n). For a balanced tree, it would be O(log n).
+
+ var invertTree = function(root){
+    if (root === null) return null;
+    [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+    return root;
+}
+
+
+// #endregion 
 
 // #region 1 -- Feb 20 2025
 
