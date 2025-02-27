f6c21cc51d51d9655845c408409bb65cfccc4954
Jem Dawson <thejemmeh@gmail.com>
2025-02-27 07:22:46 -0800

    recode

--- Javascript/704-BinarySearch/Recodes.js
+++ Javascript/704-BinarySearch/Recodes.js
@@ -1,3 +1,26 @@
+// #region 9 -- Feb 27 2025 -- Got it one go, no errors while explaining it outloud.
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ 
+ // Time Complexity: O(log n) where n is the number of elements in the nums array. We cut the array in half each time as we search for the target.
+ // Space Complexity: O(1) Because no matter the size of the nums array we just need the 3 pointers low, high, and mid to search through it. 
+ var search = function(nums, target) {
+    let low = 0,
+        high = nums.length - 1;
+    for (let i = 0; i < nums.length; i++){
+        let mid = Math.floor((high + low) / 2);
+        if(target === nums[i]) return i;
+        else if (target < nums[i]) high = mid - 1;
+        else low = mid + 1;
+    }
+    return -1;
+};
+
+// #endregion 
+
 // #region 8 -- Feb 23 2025 - First time getting it almost entirely by myself. Small syntax mistake I had to fix. 
 // Time Complexity: O(log n) - Where n is the number of elements in the nums array. We divide the array in half each time as we search for target.
 // Space Complexity: O(1) - No matter the size of the nums array we just have the same 3 pointers low, mid, and high. 
