7b1b91e98be7826ab23f7e1907af9235dba5452f
Jem Dawson <thejemmeh@gmail.com>
2025-05-10 22:32:12 -0700

    recode

---
+++ Javascript/49-GroupAnagrams/recodes.js
@@ -0,0 +1,28 @@
+// #region 1 -- May 10 2025
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+ /* Time Complexity: O(n * m * log(m)) 
+ where n is the number of strings in the input array strs. 
+ m is the average length of a string in strs.
+ string.split('').sort().join('') takes O(m * log(m)) and then you have to do it 
+ for every element in the strs array.
+
+ Space Complexity: O(n * m) 
+ The hash map will store n keys of length m. 
+ */
+ var groupAnagrams = function(strs) {
+    let hashMap = new Map();
+
+    for (let s of strs){
+        const key = s.split('').sort().join('');
+        if (!hashMap[key]) hashMap[key] = [];
+        hashMap[key].push(s);
+    }
+    return Object.values(hashMap);
+};
+
+// #endregion 
