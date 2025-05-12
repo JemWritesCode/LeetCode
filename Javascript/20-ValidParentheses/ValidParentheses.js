cd37ad6a6377a19ef4f2415ebabc5b3a4ec53b6b
Jem Dawson <thejemmeh@gmail.com>
2025-05-11 21:16:34 -0700

    valid parentheses update comments

--- Javascript/20-ValidParentheses/ValidParentheses.js
+++ Javascript/20-ValidParentheses/ValidParentheses.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 
- // Time Complexity: O(n) where n is the number of characters in s. You must traverse the entire string and put it into the stack.
- // Space Complexity: O(n) where n is the number of characters in s. In the worst case all characters are opening brackets so they all end up in the stack.
- var isValid = function(s) {
+ // Time Complexity: O(n) where n is the number of characters in the s string. We must traverse the entire string to add it to the stack. 
+// Space Complexity: O(n) where n is the number of characters in the s string. In the worst case all of the characters are opening brackets so they all end up in the stack. 
+var isValid = function(s) {
     const bracketMap = {
         '(' : ')',
         '{' : '}',
@@ -13,10 +13,12 @@
     };
     const stack = [];
     for (let c of s){
-        if (bracketMap[c])
-            stack.push(bracketMap[c]);
-        else
-            if(stack.pop() !== c) return false;
+        // if it's an opening parenthses 
+        if (bracketMap[c]){  c is the opening parentheses, bracketMap[c] gets the value so the closing.
+            stack.push(bracketMap[c]); // push the closing parentheses into the stack.
+        }
+        else // if it's a closing parenthses
+            if(stack.pop() !== c) return false; // pop the last one off the stack (tells us what to expect) and see if it matches c. if it doesn't match expectation this isn't a valid string. 
     }
-    return stack.length === 0;
+    return stack.length === 0; // if everything opened closed there should be nothing left in a valid string
 };
