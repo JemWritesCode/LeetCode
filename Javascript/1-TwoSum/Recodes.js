05d58613e1e99a1e5728523f61c28e56f03ec31b
Jem Dawson <thejemmeh@gmail.com>
2025-05-13 23:36:02 -0700

    recode

--- Javascript/1-TwoSum/Recodes.js
+++ Javascript/1-TwoSum/Recodes.js
@@ -1,3 +1,26 @@
+// #region 19 -- May 13 2025
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+ // Time Complexity: O(n) where n is the number of elements in the nums array. In worst case the needed diff is at the end of the array so we traverse the entire array looking for it.
+ // Space Complexity: O(n) where n is the number of elements in the nums array. In worse case scenario the needed diff is at teh ened of the array so we add every element of the nums array to the map as we look for it. 
+ var twoSum = function(nums, target) {
+    // every num there's a difference from the target that will add up to the target. so num + diff = target.
+    // we'll store this in a hash map so that as we go along we have the diff.
+    const map = new Map();
+    for(let i = 0; i < nums.length; i++){
+        let diff = target - nums[i];
+        if(map.has(diff)) return [map.get(diff), i];
+        map.set(nums[i],i);
+    }
+    throw new Error('No solution found for twoSum');
+};
+
+// #endregion 
+
 // #region 18 -- May 12 2025
 /**
  * @param {number[]} nums
