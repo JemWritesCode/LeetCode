863b3478f95b8bdb72558a97ca801441f6106d12
Jem Dawson <thejemmeh@gmail.com>
2025-05-27 07:44:08 -0700

    recode

--- Javascript/242-ValidAnagram/Recodes.js
+++ Javascript/242-ValidAnagram/Recodes.js
@@ -1,3 +1,22 @@
+// #region 25 -- May 27 2025
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+    let freq = new Array(26).fill(0);
+    for(let i = 0; i < s.length; i++){
+        freq[s.charCodeAt(i) - 97]++;
+        freq[t.charCodeAt(i) -97]--;
+    }
+    return freq.every(count => count === 0);
+};
+
+// #endregion 
+
 // #region 24 -- May 21 2025 -- small syntax error I had to fix but I explained it outloud okay. 
 /**
  * @param {string} s
