8993a07a54cb517f56a13893e7fe2b4b8bc3a18c
Jem Dawson <thejemmeh@gmail.com>
2025-07-11 07:21:26 -0700

    Update recodes.js

--- Javascript/226-InvertBinaryTree/recodes.js
+++ Javascript/226-InvertBinaryTree/recodes.js
@@ -1,3 +1,26 @@
+// #region 4 -- July 11 2025
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+// Time Complexity: O(n) where n is the number of nodes in the tree. We must visit each node to invert it.
+//Space Complexity: O(h)
+var invertTree = function (root) {
+  if (!root) return null; // The cake and the tree are a lie
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  return root;
+};
+
+// #endregion
+
 // #region 3 -- May 12 2025
 /**
  * Definition for a binary tree node.
@@ -12,16 +35,16 @@
  * @return {TreeNode}
  */
 
- // Depth First Search, Destructuring assignment and recursion 
- // Time Complexity: O(n) where n is the number of nodes in the tree. We must traverse the entire tree to invert it.
- // Space Complexity: O(h) Each recursive call adds a new frame to the call stack, which can lead to O(h) space complexity, where h is the height of the tree. In the worst case (for a skewed tree) this would be O(n). For a balanced tree it would be O(log n). 
- var invertTree = function(root) {
-    if (!root) return null;
-    [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
-    return root;
+// Depth First Search, Destructuring assignment and recursion
+// Time Complexity: O(n) where n is the number of nodes in the tree. We must traverse the entire tree to invert it.
+// Space Complexity: O(h) Each recursive call adds a new frame to the call stack, which can lead to O(h) space complexity, where h is the height of the tree. In the worst case (for a skewed tree) this would be O(n). For a balanced tree it would be O(log n).
+var invertTree = function (root) {
+  if (!root) return null;
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  return root;
 };
 
-// #endregion 
+// #endregion
 
 // #region 2 -- Feb 24 2025
 /**
@@ -36,19 +59,18 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
- 
- // Destructuring assignment and recursion.
- // Time Complexity: O(n) Where n is the number of nodes in the tree. We must traverse the entire tree to invert it.
- // Space Complexity: O(h) Each recursive call adds a new frame to the call stack, which can lead to O(h) space complexity, where h is the height of the tree. In the worst case (for a skewed tree), this could be O(n). For a balanced tree, it would be O(log n).
 
- var invertTree = function(root){
-    if (root === null) return null;
-    [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
-    return root;
-}
+// Destructuring assignment and recursion.
+// Time Complexity: O(n) Where n is the number of nodes in the tree. We must traverse the entire tree to invert it.
+// Space Complexity: O(h) Each recursive call adds a new frame to the call stack, which can lead to O(h) space complexity, where h is the height of the tree. In the worst case (for a skewed tree), this could be O(n). For a balanced tree, it would be O(log n).
 
+var invertTree = function (root) {
+  if (root === null) return null;
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  return root;
+};
 
-// #endregion 
+// #endregion
 
 // #region 1 -- Feb 20 2025
 
@@ -64,15 +86,15 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
- 
- // Destructuring assignment and recursion.
- // Time Complexity: O(n) Where n is the number of nodes in the tree. We must traverse the entire tree to invert it.
- // Space Complexity: O(h) Each recursive call adds a new frame to the call stack, which can lead to O(h) space complexity, where h is the height of the tree. In the worst case (for a skewed tree), this could be O(n). For a balanced tree, it would be O(log n).
 
- var invertTree = function(root){
-    if (root === null) return null;
-    [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
-    return root;
-}
+// Destructuring assignment and recursion.
+// Time Complexity: O(n) Where n is the number of nodes in the tree. We must traverse the entire tree to invert it.
+// Space Complexity: O(h) Each recursive call adds a new frame to the call stack, which can lead to O(h) space complexity, where h is the height of the tree. In the worst case (for a skewed tree), this could be O(n). For a balanced tree, it would be O(log n).
 
-// #endregion 
+var invertTree = function (root) {
+  if (root === null) return null;
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  return root;
+};
+
+// #endregion
