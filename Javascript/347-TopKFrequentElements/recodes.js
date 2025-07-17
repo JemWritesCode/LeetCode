f1fc1f1c79245dabf256ade0fde01e4483531c3f
Jem Dawson <thejemmeh@gmail.com>
2025-07-16 21:22:36 -0700

    Update recodes.js

--- Javascript/347-TopKFrequentElements/recodes.js
+++ Javascript/347-TopKFrequentElements/recodes.js
@@ -1,3 +1,30 @@
+// #region 2 -- July 16 2025
+// I knew the concept but struggled with implementation a bit but also working on this  late
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  let map = new Map();
+  const bucket = [];
+  const result = [];
+
+  for (let num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+  for (let [num, freq] of map) {
+    bucket[freq] = (bucket[freq] || new Set()).add(num);
+  }
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (bucket[i]) result.push(...bucket[i]);
+    if (result.length === k) break;
+  }
+  return result;
+};
+
+// #endregion
+
 // #region 1 -- May 20 2025
 /**
  * @param {number[]} nums
@@ -5,24 +32,24 @@
  * @return {number[]}
  */
 
- // Time Complexity O(n)
- // Space Complexity O(n)
- var topKFrequent = function(nums, k) {
-    const freqMap = new Map();
-    const bucket = [];
-    const result = [];
+// Time Complexity O(n)
+// Space Complexity O(n)
+var topKFrequent = function (nums, k) {
+  const freqMap = new Map();
+  const bucket = [];
+  const result = [];
 
-    for (let num of nums){
-        freqMap.set(num, (freqMap.get(num) || 0) + 1);
-    }
-    for (let[num, freq] of freqMap){
-        bucket[freq] = (bucket[freq] || new Set()).add(num);
-    }
-    for (let i = bucket.length - 1; i >= 0; i--){
-        if(bucket[i]) result.push(...bucket[i]);
-        if(result.length === k) break;
-    }
-    return result;
+  for (let num of nums) {
+    freqMap.set(num, (freqMap.get(num) || 0) + 1);
+  }
+  for (let [num, freq] of freqMap) {
+    bucket[freq] = (bucket[freq] || new Set()).add(num);
+  }
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (bucket[i]) result.push(...bucket[i]);
+    if (result.length === k) break;
+  }
+  return result;
 };
 
 // #endregion
