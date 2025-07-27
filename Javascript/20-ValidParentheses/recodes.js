bdadba00d9d402d9e2a93042ce8c8882f402d0d2
Jem Dawson <thejemmeh@gmail.com>
2025-07-27 10:26:07 -0700

    recodes

--- Javascript/20-ValidParentheses/recodes.js
+++ Javascript/20-ValidParentheses/recodes.js
@@ -1,22 +1,44 @@
-// #region 8 -- May 22 2025
+// #region 9 -- July 27 2025
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 
-
-var isValid = function(s){
-    const bracketMap = {
-        '(' : ')',
-        '{' : '}',
-        '[' : ']'
-    };
-    const stack = [];
-    for (let c of s){
-        if(bracketMap[c]) stack.push(bracketMap[c]);
-        else if(stack.pop() !== c) return false;
-    }
-    return stack.length === 0;
-}
+// Time Complexity: O(n) where n is the number of characters in the s string. We must go through each character in order to check for opening and closing.
+// Space Complexity: O(n) where n is the number of characters in the s string. In the worst case all of the characters in the s string are opening brackets so we end up with a stack equal to the size of s full of their corrosponding closing brackets.
+var isValid = function (s) {
+  const bracketMap = {
+    "{": "}",
+    "(": ")",
+    "[": "]",
+  };
+  let stack = [];
+  for (let c of s) {
+    if (bracketMap[c]) stack.push(bracketMap[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return stack.length === 0;
+};
 
 // #endregion
 
+// #region 8 -- May 22 2025
+
+var isValid = function (s) {
+  const bracketMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const stack = [];
+  for (let c of s) {
+    if (bracketMap[c]) stack.push(bracketMap[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return stack.length === 0;
+};
+
+// #endregion
 
 // #region 7 -- May 11 2025
 /**
@@ -24,25 +46,23 @@ var isValid = function(s){
  * @return {boolean}
  */
 
- // Time Complexity: O(n) where n is the number of characters in the s string. We must traverse the entire string to add it to the stack. 
-// Space Complexity: O(n) where n is the number of characters in the s string. In the worst case all of the characters are opening brackets so they all end up in the stack. 
-var isValid = function(s) {
-    const bracketMap = {
-        '(' : ')',
-        '{' : '}',
-        '[' : ']'
-    };
-    const stack = [];
-    for (let c of s){
-        if (bracketMap[c])
-            stack.push(bracketMap[c]);
-        else
-            if(stack.pop() !== c) return false;
-    }
-    return stack.length === 0; // if everything opened closed there should be nothing left in a valid string
+// Time Complexity: O(n) where n is the number of characters in the s string. We must traverse the entire string to add it to the stack.
+// Space Complexity: O(n) where n is the number of characters in the s string. In the worst case all of the characters are opening brackets so they all end up in the stack.
+var isValid = function (s) {
+  const bracketMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const stack = [];
+  for (let c of s) {
+    if (bracketMap[c]) stack.push(bracketMap[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return stack.length === 0; // if everything opened closed there should be nothing left in a valid string
 };
 
-// #endregion 
+// #endregion
 
 // #region 6 -- March 30 2025
 /**
@@ -51,23 +71,21 @@ var isValid = function(s) {
  */
 
 // Time Complexity: O(n) where n is the number of characters in s. We have to traverse the entire string and push each character to the stack.
-// Space Complexity: O(n) where n is the number of characters in s. In the worst case scenario we have a string of all opening brackets so they all get copied over to the stack. 
+// Space Complexity: O(n) where n is the number of characters in s. In the worst case scenario we have a string of all opening brackets so they all get copied over to the stack.
 
-var isValid = function(s){
-    const bracketMap = {
-        '(' : ')',
-        '{' : '}',
-        '[' : ']'
-    };
-    const stack = [];
-    for (let c of s){
-        if(bracketMap[c])
-            stack.push(bracketMap[c]);
-        else
-            if (stack.pop() !== c) return false;
-    }
-    return stack.length === 0;
-}
+var isValid = function (s) {
+  const bracketMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const stack = [];
+  for (let c of s) {
+    if (bracketMap[c]) stack.push(bracketMap[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return stack.length === 0;
+};
 
 // #endregion
 
@@ -78,24 +96,21 @@ var isValid = function(s){
  * @return {boolean}
  */
 
-
 // Time Complexity: O(n) where n is the number of characters in s. We have to traverse the entire string and push each character to the stack.
-// Space Complexity: O(n) where n is the number of characters in s. In the worst case scenario we have a string of all opening brackets so they all get copied over to the stack. 
-var isValid = function(s){
-    const bracketMap = {
-        '(' : ')',
-        '{' : '}',
-        '[' : ']'
-    };
-    const stack = [];
-    for (let c of s){
-        if (bracketMap[c])
-            stack.push(bracketMap[c]);
-        else
-            if (stack.pop() !== c) return false;
-    }
-    return stack.length === 0;
-}
+// Space Complexity: O(n) where n is the number of characters in s. In the worst case scenario we have a string of all opening brackets so they all get copied over to the stack.
+var isValid = function (s) {
+  const bracketMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const stack = [];
+  for (let c of s) {
+    if (bracketMap[c]) stack.push(bracketMap[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return stack.length === 0;
+};
 
 // #endregion
 
@@ -105,23 +120,21 @@ var isValid = function(s){
  * @return {boolean}
  */
 
-// Time Complexity: O(n) where n is the number of characters in s. You must traverse the entire string and put it inot the stack. 
+// Time Complexity: O(n) where n is the number of characters in s. You must traverse the entire string and put it inot the stack.
 // Space Complexity: O(n) where n is the number of characters in s. In the worst case all of the characters are opening brackets so they all end up in the stack.
-var isValid = function(s){
-    const bracketMap = {
-        '(' : ')',
-        '{' : '}', 
-        '[' : ']'
-    };
-    const stack = [];
-    for (let c of s){
-        if (bracketMap[c])
-            stack.push(bracketMap[c]);
-        else
-            if (stack.pop() !== c) return false;
-    }
-    return stack.length === 0;
-}
+var isValid = function (s) {
+  const bracketMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const stack = [];
+  for (let c of s) {
+    if (bracketMap[c]) stack.push(bracketMap[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return stack.length === 0;
+};
 
 // #endregion
 // #region 3 -- Feb 16 2025
@@ -130,24 +143,22 @@ var isValid = function(s){
  * @return {boolean}
  */
 
- // Time Complexity: O(n) where n is the number of characters in s. You must traverse the entire string and put it into the stack.
- // Space Complexity: O(n) where n is the number of characters in s. In the worst case all characters are opening brackets so they all end up in the stack.
+// Time Complexity: O(n) where n is the number of characters in s. You must traverse the entire string and put it into the stack.
+// Space Complexity: O(n) where n is the number of characters in s. In the worst case all characters are opening brackets so they all end up in the stack.
 
- isValid = function (s){
-    const bracketMap = {
-        '(' : ')',
-        '{' : '}',
-        '[' : ']'
-    };
-    const stack = [];
-    for (let c of s){
-        if (bracketMap[c])
-            stack.push(bracketMap[c]);
-        else
-            if (stack.pop() !== c) return false;
-    }
-    return stack.length === 0;
-}
+isValid = function (s) {
+  const bracketMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const stack = [];
+  for (let c of s) {
+    if (bracketMap[c]) stack.push(bracketMap[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return stack.length === 0;
+};
 
 // #endregion
 
@@ -157,25 +168,23 @@ var isValid = function(s){
  * @return {boolean}
  */
 
- // Time Complexity: O(n) where n is the number of characters in s. You must traverse the entire string and put it into the stack.
- // Space Complexity: O(n) where n is the number of characters in s. In the worst case all characters are opening brackets so they all end up in the stack.
- var isValid = function(s){
-    const bracketMap = {
-        '(' : ')',
-        '{' : '}',
-        '[' : ']'
-    };
-    const stack = [];
-    for (let c of s){
-        if (bracketMap[c])
-            stack.push(bracketMap[c]);
-        else
-            if(stack.pop() !== c) return false;
-    }
-    return stack.length === 0;
-}
+// Time Complexity: O(n) where n is the number of characters in s. You must traverse the entire string and put it into the stack.
+// Space Complexity: O(n) where n is the number of characters in s. In the worst case all characters are opening brackets so they all end up in the stack.
+var isValid = function (s) {
+  const bracketMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const stack = [];
+  for (let c of s) {
+    if (bracketMap[c]) stack.push(bracketMap[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return stack.length === 0;
+};
 
-// #endregion 
+// #endregion
 
 // #region 1 -- Feb 11 2025
 /**
@@ -183,22 +192,20 @@ var isValid = function(s){
  * @return {boolean}
  */
 
- // Time Complexity: O(n) where n is the number of characters in s. You must traverse the entire string and put it into the stack.
- // Space Complexity: O(n) where n is the number of characters in s. In the worst case all characters are opening brackets so they all end up in the stack.
- var isValid = function(s) {
-    const bracketMap = {
-        '(' : ')',
-        '{' : '}',
-        '[' : ']'
-    };
-    const stack = [];
-    for (let c of s){
-        if (bracketMap[c])
-            stack.push(bracketMap[c]);
-        else
-            if(stack.pop() !== c) return false;
-    }
-    return stack.length === 0;
+// Time Complexity: O(n) where n is the number of characters in s. You must traverse the entire string and put it into the stack.
+// Space Complexity: O(n) where n is the number of characters in s. In the worst case all characters are opening brackets so they all end up in the stack.
+var isValid = function (s) {
+  const bracketMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const stack = [];
+  for (let c of s) {
+    if (bracketMap[c]) stack.push(bracketMap[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return stack.length === 0;
 };
 
 // #endregion
--- Javascript/21-MergeTwoSortedLists/recodes.js
+++ Javascript/21-MergeTwoSortedLists/recodes.js
@@ -1,3 +1,38 @@
+// #region 6 -- July 27 2025 -- got it in one go
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+// Time Complexity: O(n + m) where n is the number of elements in list1 and m is the number of elements in list 2. We must visit each node a maximum of 1 time in order to add them to the new list.
+// Space Complexity: O(1) No matter the size of the lists passed in we only need the dummy and curr variables. For asymptotic notation we generally only consider auxiliary memory so we don't count the input or output linked lists.
+var mergeTwoLists = function (list1, list2) {
+  let dummy = new ListNode();
+  let curr = dummy;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      curr.next = list1;
+      list1 = list1.next;
+    } else {
+      curr.next = list2;
+      list2 = list2.next;
+    }
+    curr = curr.next;
+  }
+  curr.next = list1 || list2;
+  return dummy.next;
+};
+
+// #endregion
+
 // #region 5 -- July 26 2025 -- Almost by myself just forgot to .val
 /**
  * Definition for singly-linked list.
