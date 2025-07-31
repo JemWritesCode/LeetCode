1217b45e55b21f1929f96be77f23e67e743ddae4
Jem Dawson <thejemmeh@gmail.com>
2025-07-30 21:33:12 -0700

    Update Recodes.js

--- Javascript/242-ValidAnagram/Recodes.js
+++ Javascript/242-ValidAnagram/Recodes.js
@@ -1,3 +1,21 @@
+// #region 30 -- July 30 2025
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  let freq = new Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    freq[s.charCodeAt(i) - 97]--;
+    freq[t.charCodeAt(i) - 97]++;
+  }
+  return freq.every((count) => count === 0);
+};
+
+//#endregion
+
 // #region 29 -- July 7 2025
 /**
  * @param {string} s
