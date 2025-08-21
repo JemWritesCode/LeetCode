ee0a0586bbde0a60aeb143247b040a4801cc7837
Jem Dawson <thejemmeh@gmail.com>
2025-08-21 07:49:59 -0700

    Update recodes.js

--- Javascript/347-TopKFrequentElements/recodes.js
+++ Javascript/347-TopKFrequentElements/recodes.js
@@ -1,3 +1,31 @@
+// #region 16 -- Aug 21 2025 -- one go
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const freqMap = new Map();
+    const bucket = [];
+    const result = [];
+
+    for(let num of nums){
+        freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    }
+
+    for(let [num, freq] of freqMap){
+        bucket[freq] = (bucket[freq] || new Set()).add(num);
+    }
+
+    for(let i = bucket.length - 1; i >=0; i--){
+        if(bucket[i]) result.push(...bucket[i]);
+        if(result.length === k) break;
+    }
+    return result;
+}; 
+
+//#endregion
+
 // #region 16 -- Aug 17 2025
 
 /**
