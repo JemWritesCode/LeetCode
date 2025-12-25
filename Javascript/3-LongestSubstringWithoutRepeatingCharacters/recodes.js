a0b5a5846b8a8ef676c47cf062ef3ce5c535173b
Jem Dawson <thejemmeh@gmail.com>
2025-12-24 18:05:44 -0800

    recode

--- Javascript/3-LongestSubstringWithoutRepeatingCharacters/recodes.js
+++ Javascript/3-LongestSubstringWithoutRepeatingCharacters/recodes.js
@@ -1,3 +1,28 @@
+// #region 2 -- Dec 24 2025
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+//Time Complexity: O(n)
+// Space Complexity: O(n)
+var lengthOfLongestSubstring = function (s) {
+  let set = new Set();
+  let left = 0,
+    max = 0;
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[right]);
+    max = Math.max(max, right - left + 1);
+  }
+  return max;
+};
+
+//#endregion
+
 // #region 1 -- July 12 2025
 /**
  * @param {string} s
