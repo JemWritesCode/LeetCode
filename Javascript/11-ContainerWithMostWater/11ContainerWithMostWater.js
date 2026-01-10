0d43ea77d782ff9122fd51cc81c19cd15bf49141
Jem Dawson <thejemmeh@gmail.com>
2026-01-09 22:32:49 -0800

    recode

--- Javascript/11-ContainerWithMostWater/11ContainerWithMostWater.js
+++ Javascript/11-ContainerWithMostWater/11ContainerWithMostWater.js
@@ -12,17 +12,15 @@ We're just iterating over the height number array with a set number of pointers.
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    let result = 0,
-        left = 0,
-        right = height.length -1;
-
-    while (left < right){
-        let smallestSide = Math.min(height[left], height[right]);
-        let area = (right - left) * smallestSide;
-        if(area > result) result = area;
-        if(height[left] < height[right]) left++;
-        else right--;
-    }
-    return result;
+var maxArea = function (height) {
+  let result = 0,
+    left = 0,
+    right = height.length - 1;
+  while (left < right) {
+    let smallestSide = Math.min(height[left], height[right]);
+    let area = (right - left) * smallestSide;
+    if (area > result) result = area;
+    height[left] < height[right] ? left++ : right--;
+  }
+  return result;
 };
--- Javascript/11-ContainerWithMostWater/recodes.js
+++ Javascript/11-ContainerWithMostWater/recodes.js
@@ -1,20 +1,42 @@
-// #region Jan 15 2025
+// #region 2 -- Jan 9 2026
 /**
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    let result = 0,
-        left = 0, 
-        right = height.length -1;
 
-    while(left < right){
-        let smallestSide = Math.min(height[left], height[right]);
-        let area = (right - left) * smallestSide;
-        if(area > result) result = area;
-        if(height[left] < height[right]) left++;
-        else right--;
-    }
-    return result;
-}; 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+var maxArea = function (height) {
+  let result = 0,
+    left = 0,
+    right = height.length - 1;
+  while (left < right) {
+    let smallestSide = Math.min(height[left], height[right]);
+    let area = (right - left) * smallestSide;
+    if (area > result) result = area;
+    height[left] < height[right] ? left++ : right--;
+  }
+  return result;
+};
+// #endregion
+
+// #region 1 -- Jan 15 2025
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let result = 0,
+    left = 0,
+    right = height.length - 1;
+
+  while (left < right) {
+    let smallestSide = Math.min(height[left], height[right]);
+    let area = (right - left) * smallestSide;
+    if (area > result) result = area;
+    if (height[left] < height[right]) left++;
+    else right--;
+  }
+  return result;
+};
 // #endregion
