d572dd253717ef9a91c8aacfcd43e3a82ae5e61d
Jem Dawson <thejemmeh@gmail.com>
2026-01-24 16:37:29 -0800

    169

---
+++ Javascript/169-MajorityElement/169.MajorityElement.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  const count = new Map();
+  let res = 0,
+    maxCount = 0;
+  for (let num of nums) {
+    count.set(num, (count.get(num) || 0) + 1);
+    if (count.get(num) > maxCount) {
+      res = num;
+      maxCount++;
+    }
+  }
+  return res;
+};
