b2be8300a395043ed32e3d002cd3e636b5cf8275
Jem Dawson <thejemmeh@gmail.com>
2026-04-06 22:22:40 -0700

    Update Recodes.js

--- Javascript/1-TwoSum/Recodes.js
+++ Javascript/1-TwoSum/Recodes.js
@@ -1,3 +1,25 @@
+// #region 35 -- April 6, 2026
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// Time Complexity: O(n) where n is the number of elements in the nums array. In the worst case scenario the needed diff is the last element so we traverse the entire array looking for it.
+// Space Complexity: O(n) where n is the number of elements in the nums array. In the worst case scenario the needed diff is the last element so the Hashmap becomes a copy of the nums array.
+var twoSum = function (nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (map.has(diff)) return [i, map.get(diff)];
+    map.set(nums[i], i);
+  }
+  throw new Error("No solution found for TwoSum");
+};
+
+//#endregion
+
 // #region 34 -- Feb 11 2026
 
 /**
