7543a5c7574b43d8c354aa99a5cc03251e5535a4
Jem Dawson <thejemmeh@gmail.com>
2026-05-13 21:10:26 -0700

    Update Recodes.js

--- Javascript/217-ContainsDuplicate/Recodes.js
+++ Javascript/217-ContainsDuplicate/Recodes.js
@@ -1,3 +1,21 @@
+// #region 50 -- May 13, 2026
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+// Time Complexity: O(n) where n is the number of elements in the nums array. In the worst case scenario there are no duplicates and we traverse the entire array looking for one.
+// Space Complexity: O(n) where n is the number of elements in the nums array. In the worst case scenario there are no duplicates and set becomes a copy of the nums array.
+var containsDuplicate = function (nums) {
+  let set = new Set();
+  for (let num of nums) {
+    if (set.has(num)) return true;
+    set.add(num);
+  }
+  return false;
+};
+
+//#endregion
+
 // #region 49 -- April 4, 2026
 /**
  * @param {number[]} nums
