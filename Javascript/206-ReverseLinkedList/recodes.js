508a5e9fcf68f2eec0ace986b9b592c1c068cb7e
Jem Dawson <thejemmeh@gmail.com>
2026-06-18 23:08:14 -0700

    Update recodes.js

--- Javascript/206-ReverseLinkedList/recodes.js
+++ Javascript/206-ReverseLinkedList/recodes.js
@@ -1,3 +1,29 @@
+// #region 34 -- June 18, 2025
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+};
+
+// #endregion
+
 // #region 33 -- Dec 13 2025
 /**
  * Definition for singly-linked list.
