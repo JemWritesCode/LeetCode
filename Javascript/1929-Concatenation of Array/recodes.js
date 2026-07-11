53852708dfcf977a93212f4d73a9e5a045f64b38
Jem Dawson <thejemmeh@gmail.com>
2026-07-10 23:20:30 -0700

    Update recodes.js

--- Javascript/1929-Concatenation of Array/recodes.js
+++ Javascript/1929-Concatenation of Array/recodes.js
@@ -1,3 +1,14 @@
+// #region 4 -- July 10, 2026
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+  return [...nums, ...nums];
+};
+
+//#endregion
+
 // #region 3 -- May 3, 2026
 /**
  * @param {number[]} nums
