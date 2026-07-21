cb0041ff675ba38e5e3c6b89ffb7f7756d649f6d
Jem Dawson <thejemmeh@gmail.com>
2026-07-21 07:13:32 -0700

    Update Recodes.js

--- Javascript/217-ContainsDuplicate/Recodes.js
+++ Javascript/217-ContainsDuplicate/Recodes.js
@@ -1,3 +1,20 @@
+// #region 52 -- July 21, 2026
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  let set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
+  }
+  return false;
+};
+
+//#endregion
+
 // #region 51 -- July 10, 2026
 /**
  * @param {number[]} nums
